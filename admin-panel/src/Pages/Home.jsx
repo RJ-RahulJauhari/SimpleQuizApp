b2f3323d8024f08f5fa76b022048936c61a84fc8
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2024-05-15 01:57:15 +0530

    Bar Color changed

--- admin-panel/src/Pages/Home.jsx
+++ admin-panel/src/Pages/Home.jsx
@@ -61,8 +61,8 @@ const Home = () => {
                 datasets: [{
                     label: 'Matched Count',
                     data: matchedCounts,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: '#0066FF',
+                    borderColor: '#0066FF',
                     borderWidth: 1
                 }]
             },
