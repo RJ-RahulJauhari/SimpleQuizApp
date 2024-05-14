0b6c2c57f613a50784e8af280a115fc10cdac2cf
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2024-05-14 23:28:35 +0530

    something

--- admin-panel/src/constants/constants.jsx
+++ admin-panel/src/constants/constants.jsx
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:9232/quiz'
+export const BASE_URL = 'https://simplequizapp.onrender.com/quiz'
 
 // 'https://simplequizapp.onrender.com/quiz'
+// 'http://localhost:9232/quiz'
